490cfb6a321ace571527e0e613d0d3e06de91e62
dion0x <xempo286@gmail.com>
2024-07-18 18:15:26 +0530

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,24 @@ import React from 'react';
 const About = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">About Us</h1>
-      <p>This website provides information on various anime shows and movies...</p>
+      <h1 className="text-2xl font-bold">About Us <br></br></h1>
+      
+      <h1 className="text-2xl font-bold">Welcome to Anime Galaxy</h1>
+      <p>
+        At Anime Galaxy, we're passionate about bringing you the latest and greatest in anime news, reviews, and discussions. Founded by anime enthusiasts with a mission to celebrate the diverse world of Japanese animation, our goal is to provide a platform where fans can discover, connect, and engage.
+      </p>
+      <p>
+        Our team of writers and creators are dedicated to delivering insightful content that not only informs but also sparks conversations within the anime community. Whether you're a seasoned otaku or just diving into the world of anime, Anime Galaxyis here to be your companion on this exciting journey.
+      </p>
+      <p>
+        As we continue to grow, we're committed to expanding our offerings and enhancing your experience. Join us in exploring the vast universe of anime and manga, and let's celebrate our shared passion together.
+      </p>
+      <p>
+        Feel free to reach out to us at anime@gmail.com with any questions, suggestions, or just to say hello! Don't forget to follow us on Anime Galaxy to stay updated on the latest news and features.
+      </p>
+      <p>
+        Thank you for visiting Anime Galaxy where anime comes to life!
+      </p>
     </div>
   );
 };
